8a159864722a70421118ac97adeee8122ca839e9
Alan Wong <captainalan@gmail.com>
2020-07-02 15:49:21 -0700

    Removing Xah settings I don't like

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -153,10 +153,6 @@ There are two things you can do about this warning:
           '(
             (tool-bar-lines . 0)
             (background-color . "honeydew")
-            (width . 106)
-            (height . 56)
-            ;; (left . 50)
-            ;; (top . 50)
             ))
   (setq initial-frame-alist '( (tool-bar-lines . 0))))
 
@@ -164,8 +160,7 @@ There are two things you can do about this warning:
       '(
         (tool-bar-lines . 0)
         (background-color . "honeydew")
-        (width . 100)
-        (height . 55)))
+        ))
 
 ;; HH====================================================================
 ;; UTF-8 as default encoding
@@ -319,54 +314,6 @@ Version 2019-11-05"
 ;;   (add-hook 'xah-elisp-mode-hook 'xah-set-proportial-font)
 ;;   (add-hook 'js-mode-hook 'xah-set-proportial-font)
 ;;   (add-hook 'css-mode-hook 'xah-set-proportial-font))
-
-;; HH====================================================================
-
-(progn
-  ;; minibuffer setup
-  (setq enable-recursive-minibuffers t)
-  (savehist-mode 1)
-  ;; big minibuffer height, for ido to show choices vertically
-  (setq max-mini-window-height 0.5)
-  ;; minibuffer, stop cursor going into prompt
-  (customize-set-variable
-   'minibuffer-prompt-properties
-   (quote (read-only t cursor-intangible t face minibuffer-prompt))))
-
-(progn
-  ;; minibuffer enhanced completion
-  (require 'icomplete)
-  (icomplete-mode 1)
-  ;; show choices vertically
-  (setq icomplete-separator "\n")
-  (setq icomplete-hide-common-prefix nil)
-  (setq icomplete-in-buffer t)
-  (define-key icomplete-minibuffer-map (kbd "<right>") 'icomplete-forward-completions)
-  (define-key icomplete-minibuffer-map (kbd "<left>") 'icomplete-backward-completions))
-
-(progn
-  ;; make buffer switch command do suggestions, also for find-file command
-  (require 'ido)
-  (ido-mode 1)
-
-  ;; show choices vertically
-  (if (version< emacs-version "25")
-      (progn
-        (make-local-variable 'ido-separator)
-        (setq ido-separator "\n"))
-    (progn
-      (make-local-variable 'ido-decorations)
-      (setf (nth 2 ido-decorations) "\n")))
-
-  ;; show any name that has the chars you typed
-  (setq ido-enable-flex-matching t)
-  ;; use current pane for newly opened file
-  (setq ido-default-file-method 'selected-window)
-  ;; use current pane for newly switched buffer
-  (setq ido-default-buffer-method 'selected-window)
-  ;; stop ido from suggesting when naming new file
-  (when (boundp 'ido-minor-mode-map-entry)
-    (define-key (cdr ido-minor-mode-map-entry) [remap write-file] nil)))
 
 ;; HH====================================================================
 
